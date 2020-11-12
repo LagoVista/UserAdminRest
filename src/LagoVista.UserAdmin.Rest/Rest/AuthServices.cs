eee7ed1ba8863e8837c201962112c5eb5985056a
Kelvin McDaniel <kelvin.mcdaniel@software-logistics.com>
2020-11-12 18:52:40 -0500

    Support for unattended kiosk.

--- src/LagoVista.UserAdmin.Rest/Rest/AuthServices.cs
+++ src/LagoVista.UserAdmin.Rest/Rest/AuthServices.cs
@@ -26,12 +26,14 @@ using LagoVista.UserAdmin.Managers;
 using LagoVista.AspNetCore.Identity.Managers;
 using LagoVista.Core.Interfaces;
 using LagoVista.UserAdmin.Interfaces.Managers;
+using LagoVista.IoT.Deployment.Admin;
 using LagoVista.UserAdmin.Resources;
 using System.Text.RegularExpressions;
 using LagoVista.IoT.Logging.Exceptions;
 using System.Net;
 using System.Collections.Generic;
 using LagoVista.UserAdmin.ViewModels.Users;
+using LagoVista.Core;
 
 namespace LagoVista.UserAdmin.Rest
 {
@@ -53,17 +55,19 @@ namespace LagoVista.UserAdmin.Rest
         private readonly IAuthTokenManager _tokenManager;
         private readonly IPasswordManager _passwordMangaer;
         private readonly SignInManager<AppUser> _signInManager;
+		private readonly IClientAppManager _clientAppManager;
 
 
-        //IMPORTANT Until this can all be refactored into the UserAdmin class this NEEDS to point to action on the Web Site.
-        public const string ACTION_RESET_PASSWORD = "/Account/ResetPassword";
+		//IMPORTANT Until this can all be refactored into the UserAdmin class this NEEDS to point to action on the Web Site.
+		public const string ACTION_RESET_PASSWORD = "/Account/ResetPassword";
 
-        public AuthServices(IAuthTokenManager tokenManager, IPasswordManager passwordManager, IAdminLogger logger, IAppUserManager appUserManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IEmailSender emailSender, IAppConfig appConfig) : base(userManager, logger)
+        public AuthServices(IAuthTokenManager tokenManager, IPasswordManager passwordManager, IAdminLogger logger, IAppUserManager appUserManager, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IEmailSender emailSender, IAppConfig appConfig, IClientAppManager clientAppManager) : base(userManager, logger)
         {
             _tokenManager = tokenManager;
             _passwordMangaer = passwordManager;
-            _signInManager = signInManager;            
-        }
+            _signInManager = signInManager;
+			_clientAppManager = clientAppManager;
+		}
 
         private Task<InvokeResult<AuthResponse>> HandleAuthRequest(AuthRequest req)
         {
@@ -132,7 +136,7 @@ namespace LagoVista.UserAdmin.Rest
         [HttpPost("/api/v1/login")]
         public async Task<InvokeResult> CookieAuthFromForm([FromForm] LoginModel model)
         {
-            Console.WriteLine("we are login now");
+            Console.WriteLine("we are logging now");
             Console.WriteLine(model.Email);
             Console.WriteLine(model.Password);
             Console.WriteLine("-------");
@@ -146,6 +150,77 @@ namespace LagoVista.UserAdmin.Rest
             {
                 return InvokeResult.FromError("Could not authenticate");
             }
+        }
+
+        /// <summary>
+        /// Auth by Form Post with Simple Email Address and Password, will set cookie rather than JWT
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost("/api/v1/loginkiosk")]
+        public async Task<InvokeResult<string>> KioskCookieAuthFromForm([FromForm] LoginModel model)
+        {
+            Console.WriteLine("we are logging now (kiosk)");
+            Console.WriteLine(model.Email);
+            Console.WriteLine(model.Password);
+            Console.WriteLine("-------");
+
+            if (model != null && !string.IsNullOrEmpty(model.Password))
+            {
+                var kioskResult = await _clientAppManager.AuthorizeAppAsync(model.Email, model.Password); /* ClientId, ApiKey */
+                if (kioskResult.Successful)
+                {
+                    var clientApp = kioskResult.Result;
+      //              var claims = new[]
+      //              {
+      //                  new Claim(ClaimsFactory.InstanceId, clientApp.DeploymentInstance.Id),
+      //                  new Claim(ClaimsFactory.InstanceName, clientApp.DeploymentInstance.Text),
+      //                  new Claim(ClaimsFactory.CurrentOrgId, clientApp.OwnerOrganization.Id),
+      //                  new Claim(ClaimsFactory.CurrentOrgName, clientApp.OwnerOrganization.Text),
+						//new Claim(ClaimsFactory.CurrentUserId, clientApp.ClientAppUser.Id),
+      //                  new Claim(ClaimTypes.NameIdentifier, clientApp.ClientAppUser.Text),
+      //                  new Claim(ClaimTypes.Surname, "system"),
+						//new Claim(ClaimTypes.GivenName, clientApp.ClientAppUser.Text),
+      //                  new Claim(ClaimsFactory.KioskId, clientApp.Kiosk.Id),
+      //                  new Claim(ClaimsFactory.EmailVerified, true.ToString()),
+      //                  new Claim(ClaimsFactory.PhoneVerfiied, true.ToString()),
+      //                  new Claim(ClaimsFactory.IsSystemAdmin, false.ToString()),
+      //                  new Claim(ClaimsFactory.IsAppBuilder, false.ToString()),
+      //                  new Claim(ClaimsFactory.IsOrgAdmin, false.ToString()),
+      //                  new Claim(ClaimsFactory.IsPreviewUser, false.ToString()),
+      //              };
+
+      //              var identity = new ClaimsIdentity(claims);
+                    var clientAppUser = new AppUser(clientApp.ClientAppUser.Id, "system")
+                    {
+                        Id = clientApp.ClientAppUser.Id,
+                        EmailConfirmed = true,
+                        PhoneNumberConfirmed = true,
+                        IsAppBuilder = false,
+                        IsOrgAdmin = false,
+                        IsPreviewUser = false,
+                        IsSystemAdmin = false,
+                        IsUserDevice = false,
+                        OwnerUser = clientApp.OwnerUser,
+                        UserName = clientApp.ClientAppUser.Id,
+						OwnerOrganization = clientApp.OwnerOrganization,
+						CurrentOrganization = clientApp.OwnerOrganization,
+					};
+
+                    try
+                    {
+                        await _signInManager.SignInAsync(clientAppUser, false);
+
+                        return InvokeResult<string>.Create(clientApp.Kiosk.Id);
+                    }
+                    catch
+                    {
+                        return InvokeResult<string>.FromError("Could not authenticate (kiosk:1)");
+                    }
+                }
+            }
+
+            return InvokeResult<string>.FromError("Could not authenticate (kiosk:2)");
         }
 
         /// <summary>
