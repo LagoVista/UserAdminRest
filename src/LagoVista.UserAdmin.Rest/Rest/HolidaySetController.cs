e5dd11229fcf6c3825fd6d168977baa700ad7c5c
Kevin D. Wolf <kevinw@software-logistics.com>
2021-01-03 10:53:19 -0500

    Add schedeudled downtime and holiday set controllers

---
+++ src/LagoVista.UserAdmin.Rest/Rest/HolidaySetController.cs
@@ -0,0 +1,130 @@
+﻿using LagoVista.Core;
+using LagoVista.Core.Models;
+using LagoVista.Core.Models.UIMetaData;
+using LagoVista.Core.Validation;
+using LagoVista.IoT.Logging.Loggers;
+using LagoVista.IoT.Web.Common.Controllers;
+using LagoVista.UserAdmin.Interfaces.Managers;
+using LagoVista.UserAdmin.Models.Orgs;
+using LagoVista.UserAdmin.Models.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace LagoVista.UserAdmin.Rest
+{
+    [Authorize]
+    public class HolidaySetController : LagoVistaBaseController
+    {
+        IHolidaySetManager _manager;
+
+        public HolidaySetController(IHolidaySetManager manager, UserManager<AppUser> userManager, IAdminLogger logger) : base(userManager, logger)
+        {
+            _manager = manager ?? throw new ArgumentNullException(nameof(manager));
+        }
+
+        /// <summary>
+        /// Holiday Set - Add
+        /// </summary>
+        /// <param name="holidaySet"></param>
+        [HttpPost("/api/holidayset")]
+        public Task<InvokeResult> AddHolidaySetAsync([FromBody] HolidaySet holidaySet)
+        {
+            return _manager.AddHolidaySetAsync(holidaySet, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Set - Update
+        /// </summary>
+        /// <param name="holidaySet"></param>
+        /// <returns></returns>
+        [HttpPut("/api/holidayset")]
+        public Task<InvokeResult> UpdateHolidaySetAsync([FromBody] HolidaySet holidaySet)
+        {
+            SetUpdatedProperties(holidaySet);
+            return _manager.UpdateHolidaySetAsync(holidaySet, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Set - Seed scheduled down time to an organization.
+        /// </summary>
+        /// <param name="holidaysetid"></param>
+        /// <param name="destinationorgid"></param>
+        /// <returns></returns>
+        [HttpPut("/api/holidayset/copytoorg/{holidaysetid}/to/{destinationorgid}")]
+        public Task<InvokeResult> CopyToOrg(string holidaysetid, string destinationorgid)
+        {
+            return _manager.CopyToOrgAsync(holidaysetid, destinationorgid, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Sets - Get for Current Org
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/holidaysets")]
+        public Task<ListResponse<HolidaySetSummary>> GetHolidaySetForOrg()
+        {
+            return _manager.GetAllHolidaySets(OrgEntityHeader, UserEntityHeader, GetListRequestFromHeader());
+        }
+
+        /// <summary>
+        /// Holiday Set - In Use
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("/api/holidayset/{id}/inuse")]
+        public Task<DependentObjectCheckResult> InUseCheckAsync(String id)
+        {
+            return _manager.CheckHolidaySetInUseAsync(id, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Set - Get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("/api/holidayset/{id}")]
+        public async Task<DetailResponse<HolidaySet>> GetHolidaySetAsync(String id)
+        {
+            var holidaySet = await _manager.GetHolidaySetAsync(id, OrgEntityHeader, UserEntityHeader);
+
+            return DetailResponse<HolidaySet>.Create(holidaySet);
+        }
+
+        /// <summary>
+        /// Holiday Set - Key In Use
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/holidayset/{key}/keyinuse")]
+        public Task<bool> GetHolidaySetKeyInUseAsync(String key)
+        {
+            return _manager.QueryKeyInUseAsync(key, OrgEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Set - Delete
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("/api/holidayset/{id}")]
+        public Task<InvokeResult> DeleteHolidaySetAsync(string id)
+        {
+            return _manager.DeleteHolidaySetAsync(id, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Holiday Set - Create New
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/holidayset/factory")]
+        public DetailResponse<HolidaySet> CreateHolidaySet()
+        {
+            var response = DetailResponse<HolidaySet>.Create();
+            response.Model.Id = Guid.NewGuid().ToId();
+            SetAuditProperties(response.Model);
+            SetOwnedProperties(response.Model);
+            return response;
+        }
+    }
+}
---
+++ src/LagoVista.UserAdmin.Rest/Rest/ScheduledDowntimeController.cs
@@ -0,0 +1,123 @@
+﻿using LagoVista.Core;
+using LagoVista.Core.Models;
+using LagoVista.Core.Models.UIMetaData;
+using LagoVista.Core.Validation;
+using LagoVista.IoT.Logging.Loggers;
+using LagoVista.IoT.Web.Common.Controllers;
+using LagoVista.UserAdmin.Interfaces.Managers;
+using LagoVista.UserAdmin.Models.Orgs;
+using LagoVista.UserAdmin.Models.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagoVista.UserAdmin.Rest
+{
+
+    [Authorize]
+    public class ScheduledDowntimeController : LagoVistaBaseController
+    {
+        readonly IScheduledDowntimeManager _manager;
+
+        public ScheduledDowntimeController(IScheduledDowntimeManager manager, UserManager<AppUser> userManager, IAdminLogger logger) : base(userManager, logger)
+        {
+            _manager = manager ?? throw new ArgumentNullException(nameof(manager));
+        }
+
+
+        /// <summary>
+        /// Scheduled Downtime - Add
+        /// </summary>
+        /// <param name="scheduledDowntime"></param>
+        [HttpPost("/api/scheduleddowntime")]
+        public Task<InvokeResult> AddScheduledDowntimeAsync([FromBody] ScheduledDowntime scheduledDowntime)
+        {
+            return _manager.AddScheduledDowntimeAsync(scheduledDowntime, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - Update
+        /// </summary>
+        /// <param name="scheduledDowntime"></param>
+        /// <returns></returns>
+        [HttpPut("/api/scheduleddowntime")]
+        public Task<InvokeResult> UpdateScheduledDowntimeAsync([FromBody] ScheduledDowntime scheduledDowntime)
+        {
+            SetUpdatedProperties(scheduledDowntime);
+            return _manager.UpdateScheduledDowntimeAsync(scheduledDowntime, OrgEntityHeader, UserEntityHeader);
+        }
+
+
+        /// <summary>
+        /// Scheduled Downtimes - Get for Current Org
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/scheduleddowntimes")]
+        public Task<ListResponse<ScheduledDowntimeSummary>> GetScheduledDowntimeForOrg()
+        {
+            return _manager.GetScheduledDowntimesForOrgAsync(OrgEntityHeader, UserEntityHeader, GetListRequestFromHeader());
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - In Use
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("/api/scheduleddowntime/{id}/inuse")]
+        public Task<DependentObjectCheckResult> InUseCheckAsync(String id)
+        {
+            return _manager.CheckScheduledDowntimeInUseAsync(id, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - Get
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("/api/scheduleddowntime/{id}")]
+        public async Task<DetailResponse<ScheduledDowntime>> GetScheduledDowntimeAsync(String id)
+        {
+            var ScheduledDowntime = await _manager.GetScheduledDowntimeAsync(id, OrgEntityHeader, UserEntityHeader);
+
+            return DetailResponse<ScheduledDowntime>.Create(ScheduledDowntime);
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - Key In Use
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/scheduleddowntime/{key}/keyinuse")]
+        public Task<bool> GetScheduledDowntimeKeyInUseAsync(String key)
+        {
+            return _manager.QueryKeyInUseAsync(key, OrgEntityHeader);
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - Delete
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("/api/scheduleddowntime/{id}")]
+        public Task<InvokeResult> DeleteScheduledDowntimeAsync(string id)
+        {
+            return _manager.DeleteScheduledDowntimeAsync(id, OrgEntityHeader, UserEntityHeader);
+        }
+
+        /// <summary>
+        /// Scheduled Downtime - Create New
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/api/scheduleddowntime/factory")]
+        public DetailResponse<ScheduledDowntime> CreateScheduledDowntime()
+        {
+            var response = DetailResponse<ScheduledDowntime>.Create();
+            response.Model.Id = Guid.NewGuid().ToId();
+            SetAuditProperties(response.Model);
+            SetOwnedProperties(response.Model);
+            return response;
+        }
+    }
+}
