c35256e4db1b0e833a2977cc745f88ef23516994
Kevin Wolf <kevinw@software-logistics.com>
2022-11-19 06:16:23 -0500

    user role service

--- src/LagoVista.UserAdmin.Rest/Rest/UserRoleService.cs
+++ src/LagoVista.UserAdmin.Rest/Rest/UserRoleService.cs
@@ -1,6 +1,7 @@
 ﻿using LagoVista.Core.Validation;
 using LagoVista.IoT.Logging.Loggers;
 using LagoVista.IoT.Web.Common.Controllers;
+using LagoVista.UserAdmin.Interfaces;
 using LagoVista.UserAdmin.Interfaces.Managers;
 using LagoVista.UserAdmin.Models.Users;
 using Microsoft.AspNetCore.Authorization;
@@ -8,6 +9,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace LagoVista.UserAdmin.Rest
@@ -27,6 +29,26 @@ namespace LagoVista.UserAdmin.Rest
         public Task<InvokeResult<UserRole>> GrantAsync(string userid, string roleid)
         {
             return _userRoleManager.GrantUserRoleAsync(userid, roleid, OrgEntityHeader, UserEntityHeader);
+        }
+
+        [HttpPost("/app/user/{userid}/roles/grant")]
+        public async Task<InvokeResult<List<UserRole>>> GrantAsync([FromBody] List<string> roles, string userid)
+        {
+            var results = new InvokeResult<List<UserRole>>() { Result = new List<UserRole>() };
+           var grantedRoles = await _userRoleManager.GrantUserRolesAsync(userid, roles, OrgEntityHeader, UserEntityHeader);
+            foreach(var result in grantedRoles)
+            {
+                if (result.Successful)
+                {
+                    results.Result.Add(result.Result);
+                }
+                else
+                {
+                    result.Errors.AddRange(result.Errors);
+                }
+            }
+
+            return results;
         }
 
         [HttpDelete("/app/user/role/revoke/{userroleid}")]
